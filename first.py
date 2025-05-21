9144ef0fd815df1158ea9812b611f29f26c0dfa5
keziashiny <keziashiny01@gmail.com>
2025-05-21 10:34:43 -0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,334 @@
+# pythonlife = " learn python"
+# print(pythonlife.startswith("n"))
+
+# pythonlife = "python language"
+# print(pythonlife.replace("language", "program"))
+
+# pythonlife = "python language"
+# #print(pythonlife.index("kezia"))
+# #print(pythonlife.find("kezia"))
+
+# #pythonlife = "python language"
+# #print(pythonlife.count('g'))
+# #print(pythonlife.removeprefix("python"))
+# #print(pythonlife.removesuffix("python"))
+
+# #pythonlife = "python language"
+# #print(pythonlife.split())
+
+
+# #pythonlife = "      python language     "
+# #print(pythonlife)
+# #print(len(pythonlife))
+# #print(pythonlife.lstrip())
+# #print(len(pythonlife))
+
+# v = [1, 2, 3, 4, 6, 1, 2, 3, 89, 'kezia']
+# #v.append("python")
+# #print(v.count(1))
+# #v.remove(1)
+# #print(v)
+# #v.pop(2)
+# #print(v)
+
+# #print(v.index(3))
+
+
+# #for i in [1,2,3,4,5, 'kezia']:
+#     #print(i)
+
+# # d = {1: 'abc', 22: 'kezia', 'pythonlife' :1}
+# #print(type(d[22]))
+# #print(d['pythonlife'])
+# #print(d.keys())
+# #print(d.values())
+# #print(d.items())
+
+# # for i in {1: 'abc', 22:'pothumudi', 'language' :1}.values():
+# #     print(i)
+
+# # s= {1,3,5,6,8,0,5,89}
+# # s.add(123)
+# # print(s)
+# # s.update({21,90,78, 67})
+# # print(s)
+# # s.pop()
+# # print(s)
+
+# # set1={1,2,3,4,5,6,7,8}
+# # set2={1,2,3,4,5,6}
+# # print(set1.issubset(set2))      
+
+
+# # def func():
+# #     print("this is function")
+# # func()    
+
+
+# # def func(a,b,c):
+# #     print("this is function",a,b,c)
+# # func(1,2,3)
+
+
+# # def func(**a):
+# #     print("this is function",a)
+# # func(a=1,b=2,c=3)    
+
+# # def outer():
+# #     print("outer fucntion")
+# #     def inner():
+# #         print("inner fucntion")
+# #     inner()
+# # outer()        
+
+# # def add(a,b):
+# #     print(a+b)
+# # def sub(a,b):
+# #     print(a-b)    
+
+# # s= open('demo.py', mode='w+')
+# # s.write("w+ mode")
+# # s.seek(0)
+# # print(s.read())
+
+# # name=input("what is your favorite colour")
+# # print("red is my favorite color! what about yours" + colour)
+
+# # name = "Alice"
+# # print("Hello, " + name)
+
+# # name = input("What is your name? ")
+# # print("Nice to meet you, " + name + "!")
+
+# # class PythonLife:
+# #     a = 2
+
+# #     def output(self):
+# #         print(self.a)
+
+# # b = PythonLife()
+# # v = PythonLife()
+# # print(b.a)
+# # b.output()
+# # v.output()
+
+
+# # colour = "red"
+# # siblings = 2
+# # GPA = 3.9
+# # python = True
+
+# # print("colour: ", type(colour))
+# # print("siblings: ", type(siblings))
+# # print("GPA: ", type(GPA))
+# # print("Python: ", type(python))
+
+# # print("Favourite colour:", colour, "| Type:", type(colour))
+# # print("Number of siblings:", siblings, "| Type:", type(siblings))
+# # print("GPA:", GPA, "| Type:", type(GPA))
+# # print("Likes Python:", python, "| Type:", type(python))
+
+
+# # x = 9
+# # y = 8
+
+# # print("Addition: ", x+y)
+# # print("subtraction: ", x-y)
+# # print("Multiplication: ", x*y)
+# # print("Division: ", x/y)
+# # print("is x Greater than y? : ", x>y)
+# # print("is y Greater than x? : ", y>x)
+# # print("Is x > 9 and y < 9? :", x>9 and y<9)
+# # print("is x == 9 or y > 8: ?", x == 9 or y > 8)  
+
+# # a = 60
+
+# # if a > 60:
+# #     print("a is positive number")
+# # elif a == 0:
+# #     print("zero")
+# # else:
+# #     print("negative number")
+
+
+# # num = int(input("enter a num: "))
+# # print("entered number: ", num)
+
+# # num = int(input("entered number: "))
+# # if num % 2 == 0:
+# #     print("even number")
+# # else:
+# #     print("odd number")   
+
+# # score = float(input("enter the score: "))
+# # if score > 90.5:
+# #     print("Grade A")
+# # elif score < 80.0:
+# #     print("Grade B+")
+# # elif score < 75.0:
+# #     print("Grade B")
+# # elif score < 70.0:
+# #     print("Grade B")
+# # else:
+# #     print("Grade C")                
+
+      
+# # num1 = int(input("Enter first the number: "))
+# # num2 = int(input("Enter second the number: "))
+# # if num1 > num2:
+# #     print("num1 is greater")
+# # elif num2 > num1:
+# #     print("num2 is grater than num1") 
+# # else:
+# #     print("Both are equal")  
+
+# # leapyear = int(input("Enter the number: "))
+# # if (leapyear % 400 == 0):
+# #     print("It is a Leap year")
+# # elif (leapyear % 100 == 0):
+# #     print("It is not a leap year")  
+# # elif (leapyear % 4 == 0):
+# #     print("It is a leap year")  
+# # else:
+# #     ("not a leap year")   
+
+# # for i in range(5):
+# #     print("hello", i)     
+
+# # count = 0
+# # while count in range(5):
+# #     print("count is:", count)
+# #     count+= 1
+# # for i in range(5):
+# #     if i == 5:
+# #         break
+# #     print(i)
+
+# # for i in range(9):
+# #     if i == 0:
+# #         continue
+# #     print(i)
+
+# # for i in range(10):
+# #     print ("count is: ", i)
+# # i += 1
+
+# # for i in range(2,21):
+# #     print("count is: ", i)
+# #     if i % 2 == 0:
+# #         print("even number")
+# #     else:
+# #         print("odd")
+
+
+# # for i in range(1,20,2):
+# #    print(i) 
+
+
+# # for i in range(1,6):
+# #     print("numbers :", i)
+
+# # for i in range(10,21):
+# #     if (i % 2 == 0): 
+# #         print("even numbers: ", i)
+
+# # for i in range(1,6):
+# #     print(f"{i} squared is {i**2}")
+
+# # name = "kezia"
+# # age = 24
+# # print(f"my name is {name} and age is {age}")
+
+# x = 10
+# y = 20
+# z = 30
+# print(f"The value of x is {x}, the value of y is {y} and the value of z is {z}.The sum of the three numbers is {x+y+z}")
+    
+# pi = 3.121342
+# print(f"pi is rounded to 2 decimal places is {pi:.2f}")
+
+# name = "kezia"
+# age = 24
+# height = 5.6
+# print(f"""
+#       Hello, my name is {name}, i am {age} years old and i am {height}""" )
+# for i in range (10, 0, -2):
+
+#         print("numbers: ", i)
+
+
+# for i in range(1,21):
+#         if (i % 2 == 1):
+#                 print("odd numbers: ", i)
+# factorial = 1               
+# for i in range(1,6):
+#         factorial = factorial * i
+    
+#         print("factorial: ", factorial)   
+
+# total = 0 
+# for i in range(1,31):
+#     if i % 3 == 0:
+#         total += i
+#         print("numbers divisible by 3: ", total)
+
+ 
+# multiplicationtable = int(input("enter the number: "))
+# for i in range(1,11):
+#     print(f"{multiplicationtable} * {i} = {multiplicationtable * i}")
+
+       
+# table = int(input("enter the number: "))
+# total = 0
+# for i in range (1,11):
+#     product = table * i
+#     print(f"{table} * {i} = {product}")
+# if (product % 2 == 0):
+#     total += product
+#     print("sum of only even results :", total)
+
+
+
+# table = int(input("enter the number: "))
+# odd_total = 0
+# for i in range(1,11):
+#     product = table * i
+#     print(f"{table} * {i} = {product}")
+#     if (product % 2 == 1):
+#          odd_total += 1
+# print("number of odd results :", odd_total)
+
+# n = int(input("enter the number of rows: "))
+# for i in range(1, n+1):
+#     print('*' * i)
+
+
+# n = int(input("enter the number of numbers: "))
+# for i in range (1, n+1):
+#     for j in range(1, i + 1):
+#       print(j, end=' ')
+#     print()  
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print('*', end=' ')
+#     print()
+
+# for i in range(2, 21):
+#     if (i % 2 == 0):
+#         print("even numbers ", i)
+
+# n = ["apple", "banana", "kiwi"]
+# for i in n:
+#     print(f"{i}: {len(i)}")
+    
+# total = 0
+# for i in range(1,51):
+#        total += i   
+# print("numbers: ", total)     
+
+n = ['1', '4' ,'7','9','10','8','90','20','40','12','17','80','25']
+for num in n:
+  if int(num) > 10:
+    print("numbers grater than 10 :", num)
